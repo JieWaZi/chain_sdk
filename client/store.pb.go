73972391a2d0220fe678fccedef7e4608eb1d302
吴俊杰 <wjj5699121@163.com>
2020-05-30 14:38:26 +0800

    删除pb的部分代码

--- client/store.pb.go
+++ client/store.pb.go
@@ -1,10 +1,5 @@
 package client
 
-type PutStateRequest struct {
-	Keys  []string `json:"keys,omitempty"`
-	Value string   `json:"value,omitempty"`
-}
-
 type Vehicle struct {
 	ObjectType string  `json:"objectType"`
 	CreateTime int64   `json:"createTime"`
@@ -14,23 +9,4 @@ type Vehicle struct {
 	OwnerId    string  `json:"ownerId"`
 	Status     int32   `json:"status"`
 	UserId     string  `json:"userId"`
-}
-
-type GetStateRequest struct {
-	Keys []string `json:"keys,omitempty"`
-}
-
-type GetHistoryRequest struct {
-	Keys []string `json:"keys,omitempty"`
-}
-
-type GetHistoryResponse struct {
-	KeyModifications []*KeyModification `json:"key_modifications,omitempty"`
-}
-
-type KeyModification struct {
-	TxId      string `json:"tx_id,omitempty"`
-	Value     string `json:"value,omitempty"`
-	Timestamp int64  `json:"timestamp,omitempty"`
-	IsDelete  bool   `json:"is_delete,omitempty"`
 }
